d805b157a9dcad77e6191826c62a2f0d08a6cb7d
Mark Hammond <mark@dsv.io>
2018-07-08 22:00:52 +0100

    added grkan import

--- graphinterface/interface/views.py
+++ graphinterface/interface/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+import grakn
 
 # Create your views here.
 
